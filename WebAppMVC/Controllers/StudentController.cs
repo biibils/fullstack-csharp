2cfece03e2d2ef6636692906480074351794b341
Billy Sutawijaya <83947150+biibils@users.noreply.github.com>
2025-12-13 08:20:22 +0000

    feat: update controller untuk Edit, Delet Student. Update program.cs

--- WebAppMVC/Controllers/StudentController.cs
+++ WebAppMVC/Controllers/StudentController.cs
@@ -1,10 +1,28 @@
+using System.ComponentModel.Design;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.Mvc;
 using WebAppMVC.Models;
+using WebAppMVC.Services;
 
 namespace WebAppMVC.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly IStudentService _studentService;
+
+        // Dependensi Injection melalui konstruktor
+        public StudentController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        public IActionResult Index()
+        {
+            var students = _studentService.GetAllStudents();
+            return ViewTechnology(students);
+        }
+        
         // GET: Student/Create
         public IActionResult Create()
         {
@@ -20,15 +38,69 @@ namespace WebAppMVC.Controllers
             {
                 // Disini untuk menyimpan data student ke database
                 // Misalnya: _context.Students.Add(student);
-                // Untuk sementara, kita hanya menampilkan data yang diterima
-                ViewBag.Message = "Data siswa berhasil disimpan!";
-                return View("Details", student);
+                _studentService.AddStudent(student);
+                return RedirectToAction(nameof(Index));
             }
             return View(student);
         }
 
+        // GET: Student/Edit/{id}
+        public IActionResult Edit(int id)
+        {
+            var student = _studentService.GetStudentById(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Student/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Student student)
+        {
+            if (id != student.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _studentService.UpdateStudent(student);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        // GET: Student/Delete/{id}
+        public IActionResult Delete(int id)
+        {
+            var student = _studentService.GetStudentById(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Student/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _studentService.DeleteStudent(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Student/Details/{id}
         public IActionResult Details(Student student)
         {
+            var student = _studentService.GetStudentById(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
             return View(student);
         }
     }
--- WebAppMVC/Program.cs
+++ WebAppMVC/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Daftarkan StudentService sebagai Scoped service
+builder.Services.AddScoped<IStudentService, StudentService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
